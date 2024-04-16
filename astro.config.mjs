30645f301bca69622897dae3d6f156cd36d105f1
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2024-04-16 20:28:38 +0200

    chore: first commit

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: {
+				github: 'https://github.com/qwidditch',
+
+			},
+			sidebar: [
+				{
+					label: 'Getting started',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Components',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
