d8920bba884796a785a1b1cf16c0fe82436f8470
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2024-04-16 20:34:12 +0200

    feat: add qwik

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,28 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import qwikdev from "@qwikdev/astro";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/qwidditch',
-
-			},
-			sidebar: [
-				{
-					label: 'Getting started',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Components',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: 'My Docs',
+    social: {
+      github: 'https://github.com/qwidditch'
+    },
+    sidebar: [{
+      label: 'Getting started',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        link: '/guides/example/'
+      }]
+    }, {
+      label: 'Components',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }), qwikdev()]
 });
