3d960e306045c33bf6a8652a36869040acf1a946
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2024-04-21 22:35:06 +0200

    feat(sidebar): add roadmap and changelog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,35 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import qwikdev from "@qwikdev/astro";
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+import qwikdev from '@qwikdev/astro'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'Qwidditch',
-    social: {
-      github: 'https://github.com/qwidditch'
-    },
-    sidebar: [{
-      label: 'Getting started',
-     	autogenerate: {
-				directory: 'getting-started'
-		 	}
-    }, {
-      label: 'Components',
-      autogenerate: {
-        directory: 'components'
-      }
-    }]
-  }), qwikdev(), tailwind()]
-});
+  integrations: [
+    starlight({
+      title: 'Qwidditch',
+      social: {
+        github: 'https://github.com/qwidditch',
+      },
+      sidebar: [
+        {
+          label: 'Getting started',
+          autogenerate: {
+            directory: 'getting-started',
+          },
+        },
+        {
+          label: 'Components',
+          autogenerate: {
+            directory: 'components',
+          },
+        },
+        { label: 'Roadmap', autogenerate: { directory: 'roadmap' } },
+        { label: 'Changelog', link: '/changelog' },
+      ],
+    }),
+    qwikdev(),
+    tailwind(),
+  ],
+})
