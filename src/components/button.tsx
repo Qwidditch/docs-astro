96b164b90463223db9b70e817338bb8785ae64c4
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2024-04-21 22:23:18 +0200

    feat(button.tsx): add custom classes

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,10 +1,11 @@
 import { component$, Slot, type PropsOf } from '@builder.io/qwik'
+import { cn } from '../utils/cn'
 
 type ButtonProps = PropsOf<'button'>
 
 export const Button = component$<ButtonProps>(({ ...props }) => {
   return (
-    <button {...props} class="btn">
+    <button {...props} class={cn('btn', props.class)}>
       <Slot />
     </button>
   )
