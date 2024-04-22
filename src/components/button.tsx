43fb461adf7d1b6ff40905e5c51e73915188ec0e
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2024-04-22 16:56:45 +0200

    feat(button): add variants color, size and variant

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,12 +1,46 @@
 import { component$, Slot, type PropsOf } from '@builder.io/qwik'
 import { cn } from '../utils/cn'
+import { cva, type VariantProps } from 'class-variance-authority'
 
-type ButtonProps = PropsOf<'button'>
-
-export const Button = component$<ButtonProps>(({ ...props }) => {
-  return (
-    <button {...props} class={cn('btn', props.class)}>
-      <Slot />
-    </button>
-  )
+const buttonVariants = cva('btn', {
+  variants: {
+    color: {
+      default: '',
+      neutral: 'btn-neutral',
+      primary: 'btn-primary',
+      secondary: 'btn-secondary',
+      accent: 'btn-accent',
+      ghost: 'btn-ghost',
+      link: 'btn-link',
+    },
+    size: {
+      xs: 'btn-xs',
+      sm: 'btn-sm',
+      md: 'btn-md',
+      lg: 'btn-lg',
+    },
+    variant: {
+      outline: 'btn-outline',
+      filled: '',
+    },
+  },
+  defaultVariants: {
+    color: 'default',
+    size: 'md',
+  },
 })
+
+type ButtonProps = PropsOf<'button'> & VariantProps<typeof buttonVariants>
+
+export const Button = component$<ButtonProps>(
+  ({ color, size, variant, ...props }) => {
+    return (
+      <button
+        {...props}
+        class={cn(buttonVariants({ color, size, variant }), props.class)}
+      >
+        <Slot />
+      </button>
+    )
+  }
+)
