58155374a6990d6cdc42fe10393446019796b9cb
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2024-04-28 10:47:20 +0200

    chore: remove github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,6 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://qwidditch.github.io',
-  base: '/docs-astro',
   integrations: [
     starlight({
       title: 'Qwidditch',
